d68565fc1b309d59b87337046ebfd4ac7992cf43
pratixanS <pratixan123@gmail.com>
2023-08-20 03:04:45 +0200

    Reading Port from .env file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pratixanS360/RSS-aggregator-project
 
 go 1.21.0
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+       "fmt"
+       "log"
+       "os"
+	   "github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Entry point to the RSS aggregator!")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+	   log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Println("PORT: ", portString)
 }
