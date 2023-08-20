88cd863c6342ee7a300919c951de68659deb0dad
pratixanS <pratixan123@gmail.com>
2023-08-20 21:55:14 +0200

    http server added

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
        "fmt"
        "log"
        "os"
+	   "net/http"
 	   "github.com/joho/godotenv"
+	   "github.com/go-chi/chi"
+	   //"github.com/go-chi/cors"
 )
 
 func main() {
@@ -17,5 +20,18 @@ func main() {
 	   log.Fatal("PORT is not found in the environment")
 	}
 
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+	}
+
+	log.Printf("Server starting on PORT %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
 	fmt.Println("PORT: ", portString)
 }
